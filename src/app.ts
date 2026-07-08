62ee512737a0895d2375b4ae039df3c727cbf8a7
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-07-08 13:49:56 +0600

    added notfound and global error handler add

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
 import cors from "cors";
+import { notFound } from "./middlewares/notFound";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 
 
 const app : Application = express();
@@ -18,6 +20,15 @@ app.use(cookieParser())
 app.get("/",(req : Request, res : Response) => {
     res.send("Hello, World!");
 });
+
+
+
+
+
+
+app.use(notFound)
+
+app.use(globalErrorHandler)
 
 
 export default app;
