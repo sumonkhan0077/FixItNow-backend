95a7377981db4b8cfb4d8869a3274eae6f07d9eb
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-07-08 21:40:03 +0600

    added jwt utils add

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -19,6 +19,18 @@ declare global {
 
 export const auth = (...requiredRoles : Role[]) => {
     return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-      
+      const token = req.cookies.accessToken ? req.cookies.accessToken 
+      :
+      req.headers.authorization?.startsWith("Bearer ")? 
+        req.headers.authorization?.split(" ")[1] 
+         : 
+        req.headers.authorization;
+        
+        
+        if(!token){
+            throw new Error("You are not logged in. Please log in to access this resource.");
+        }
+
+        const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
     })
 }
